1c24efc5f004cf0eccd510f144523559fb15e858
Anuar Alfetahe <anuar.alfetahe@gmail.com>
2023-03-26 05:21:32 +0300

    Adding new subscriber.

--- src/blockade.erl
+++ src/blockade.erl
@@ -1,6 +1,9 @@
 -module(blockade).
 
--export([subscribe/3]).
+-export([subscribe/3, emit/3]).
 
 subscribe(StatemPid, EventKey, Opts) ->
-    gen_statem:send_request(StatemPid, {add_event_subscriber, EventKey, self(), Opts}).
+    gen_statem:call(StatemPid, {add_event_subscriber, EventKey, self(), Opts}).
+
+emit(StatemPid, EventKey, EventData) ->
+    gen_statem:call(StatemPid, {event, EventKey, EventData}).
--- src/blockade_manager.erl
+++ src/blockade_manager.erl
@@ -3,21 +3,25 @@
 -behaviour(gen_statem).
 
 -export([start_link/1]).
--export([wait_event/3]).
+-export([idle/3]).
 -export([init/1, callback_mode/0]).
 
--record(state_data, {event_subscibers,events_emitted}).
+-record(state_data, {event_subscibers, events_emitted}).
 
-start_link(_) ->
-    gen_statem:start_link({local, ?MODULE}, ?MODULE, #state_data{event_subscibers = #{}, events_emitted = []}).
+start_link(#{name := Name} = _Args) ->
+    gen_statem:start_link({local, Name}, ?MODULE, [], []).
 
-init(StateData) ->
-    {ok, wait_event, StateData}.
+init(_) ->
+    StateData = #state_data{event_subscibers = #{}, events_emitted = []},
+    {ok, idle, StateData}.
 
 callback_mode() ->
-    [state_functions,state_enter].
+    [state_functions, state_enter].
 
 %%% State callbacks
 
-wait_event(_EventType, _EventContent, StateData) ->
+idle({call, From}, {add_event_subscriber, EventKey, Pid, Opts}, StateData) ->
+    NewStateData = StateData#state_data{event_subscibers = maps:put(EventKey, {Pid, Opts}, StateData#state_data.event_subscibers)},
+    {keep_state, NewStateData, {reply, From, ok}};
+idle(_EventType, _EventData, StateData) ->
     {keep_state, StateData}.
