f79522b555855611744f6f0115747e68c561641d
Anuar Alfetahe <anuar.alfetahe@gmail.com>
2023-05-02 18:50:39 +0300

    Dist test for single node.

--- test/blockade_dist_SUITE.erl
+++ test/blockade_dist_SUITE.erl
@@ -1,5 +1,7 @@
 -module(blockade_dist_SUITE).
 
+-define(NR_OF_NODES, 1).
+
 -include_lib("common_test/include/ct.hrl").
 
 -behaviour(ct_suite).
@@ -8,15 +10,23 @@
 -export([test_add_handler_dist/1]).
 
 all() ->
-    [test_add_handler_dist].
+    [test_add_handler_dist].   
 
-init_per_testcase(_TestCase, Config) ->
-    {ok, Peer, Node} = {ok, 1, 2},
+init_per_testcase(TestCase, Config) ->
+    {ok, Peer, Node} = ?CT_PEER(["-pa", code:lib_dir(blockade) ++ "/ebin"]),
     [{peer_node, {Peer, Node}} | Config].
+    %Nodes = [?CT_PEER(["-pa", code:lib_dir(blockade) ++ "/ebin"]) || _Nr <- lists:seq(1, ?NR_OF_NODES)],
+    %[rpc:call(Node, blockade_sup, start_link, [#{name => TestCase}]) || {_, _Peer, Node} <- Nodes],   
+    %[{nodes, Nodes} | Config].
 
-end_per_testcase(_TestCase, Config) ->
-    {Peer, _Node} = ?config(peer_node, Config),
-    peer:stop(Peer).
+end_per_testcase(TestCase, Config) ->
+    {Peer, Node} = ?config(peer_node, Config),
+    rpc:call(Node, blockade_sup, stop, [TestCase]),
+    peer:stop(Peer),
+    %Nodes = ?config(nodes, Config),
+    %[rpc:call(Node, blockade_sup, stop, [TestCase]) || {_, _Peer, Node} <- Nodes],   
+    %[peer:stop(Peer) || {_, Peer, _Node} <- Nodes],
+    ok.
 
 test_add_handler_dist(_Config) ->
     ok.
