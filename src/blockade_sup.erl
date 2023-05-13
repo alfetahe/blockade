acc171775d0f9796a7990ab8a63b74a1d8f4891e
Anuar Alfetahe <anuar.alfetahe@gmail.com>
2023-05-13 22:33:09 +0300

    Removed blockade delegable.

--- src/blockade_sup.erl
+++ src/blockade_sup.erl
@@ -6,18 +6,22 @@
 
 -behaviour(supervisor).
 
--export([start_link/1, stop/1]).
+-export([start_link/1, start_link/2, stop/1]).
 -export([init/1]).
 
-start_link(#{name := Name} = Args) ->
-    supervisor:start_link({local, ?PROCESS_NAME(Name, ?SUFFIX)}, ?MODULE, Args).
+start_link(Name) ->
+    start_link(Name, #{}).
+
+start_link(Name, Opts) ->
+    supervisor:start_link({local, ?PROCESS_NAME(Name, ?SUFFIX)},
+                          ?MODULE,
+                          maps:put(name, Name, Opts)).
 
 init(#{name := Name} = Args) ->
     % TODO: patternmatch all mandatory keys.
     {ok,
      {{one_for_one, 5, 10},
       [#{id => blockade_event_manager, start => {blockade_event_manager, start_link, [Args]}},
-       #{id => blockade_delegable, start => {blockade_delegable, start_link, [Args]}},
        #{id => blockade_pg, start => {pg, start_link, [?PROCESS_NAME(Name, "pg")]}}]}};
 init(_) ->
     throw(mandatory_keys_missing).
